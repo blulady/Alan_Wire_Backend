eee31294b6dadf518ae6dd2ba8fb42c26524ad64
ssanger <ssanger2020@gmail.com>
2024-05-22 21:53:19 -0700

    updated profile serializer to work with delete/profile functions in signup/admindash

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,4 @@
 class ProfileSerializer
   include JSONAPI::Serializer
-  attributes :employee_number, :first_name, :last_name, :is_admin
+  attributes :employee_number, :first_name, :last_name, :is_admin, :id
 end
